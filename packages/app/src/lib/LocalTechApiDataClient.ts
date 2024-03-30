f0048ec3acf0d67036144d1611e03ab3c80f2e1c
Ioannis Canellos <iocanel@gmail.com>
2024-03-30 12:09:58 +0200

    fixup: tsc warning in LocalTechApiDataClient.ts

--- packages/app/src/lib/LocalTechApiDataClient.ts
+++ packages/app/src/lib/LocalTechApiDataClient.ts
@@ -4,20 +4,9 @@ import {
 } from '@backstage/plugin-tech-radar';
 
 export class LocalTechApiDataClient implements TechRadarApi {
-  async load(id: string | undefined): Promise<TechRadarLoaderResponse> {
-    console.log('Loading /tech-radar.json');
-    const data = await fetch('/tech-radar.json').then(res => {
-        return res.json();
-      });
-    return {
-      ...data,
-      entries: data.entries.map(entry => ({
-        ...entry,
-        timeline: entry.timeline.map(timeline => ({
-          ...timeline,
-          date: new Date(timeline.date),
-        })),
-      })),
-    };
-  }
+  async load(_: string | undefined): Promise<TechRadarLoaderResponse> {
+    return await fetch('/tech-radar.json').then(res => {
+      return res.json();
+    });
+  };
 }
