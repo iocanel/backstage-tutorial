05051376dc73a6d852832168a63106b8ddecbc90
Ioannis Canellos <iocanel@gmail.com>
2024-04-13 20:22:01 +0300

    feat: enable tekton plugin

--- packages/app/src/components/catalog/EntityPage.tsx
+++ packages/app/src/components/catalog/EntityPage.tsx
@@ -58,6 +58,10 @@ import {
 import { TechDocsAddons } from '@backstage/plugin-techdocs-react';
 import { ReportIssue } from '@backstage/plugin-techdocs-module-addons-contrib';
 import { EntityKubernetesContent } from '@backstage/plugin-kubernetes';
+import {
+    TektonCI, isTektonCIAvailable,
+} from '@janus-idp/backstage-plugin-tekton';
+
 
 const techdocsContent = (
   <EntityTechdocsContent>
@@ -71,10 +75,12 @@ const cicdContent = (
   // This is an example of how you can implement your company's logic in entity page.
   // You can for example enforce that all components of type 'service' should use GitHubActions
   <EntitySwitch>
+    <EntitySwitch.Case if={isTektonCIAvailable}>
+      <TektonCI />
+    </EntitySwitch.Case>
     <EntitySwitch.Case if={isGithubActionsAvailable}>
       <EntityGithubActionsContent />
     </EntitySwitch.Case>
-
     <EntitySwitch.Case>
       <EmptyState
         title="No CI/CD available for this entity"
